a4a4e9522a367d10105bad0239caa5db5bac6f7b
kori2597 <57742337+kori2597@users.noreply.github.com>
2019-11-18 23:25:04 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById("submitButton").addEventListener("click", redirectToPersonalizationPage);
+
+function redirectToPersonalizationPage() {
+    window.location.href = "./personalization(1).html";
+}
